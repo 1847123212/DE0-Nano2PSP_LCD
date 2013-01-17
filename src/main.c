dafe863381cbf2ef216bde14f2099b5abeaf5f87
Morten Jensen <soaring.morten@gmail.com>
2013-01-17 14:49:29 +0100

    Cleaned the code a little and added a small header

--- src/main.c
+++ src/main.c
@@ -1,5 +1,11 @@
 /*----------------------------------------------------------------------*/
-/* FatFs sample project for generic microcontrollers (C)ChaN, 2012      */
+/* Digital picture frame project, Morten Jensen 2013      		*/
+/* I have used code for varios places for this project.			*/
+/* This is mainly the FAT lib and some code for the display.		*/
+/* The FAT lib can be found at: 					*/
+/* http://elm-chan.org/fsw/ff/00index_e.html				*/
+/* The inspiration for the display code come from: 			*/
+/* http://sites.google.com/site/fpgaandco/				*/
 /*----------------------------------------------------------------------*/
 
 #include <stdio.h>
@@ -94,41 +100,37 @@ void die (		/* Stop with dying message */
 	for (;;) ;
 }
 
-
-/*-----------------------------------------------------------------------*/
-/* Program Main                                                          */
-/*-----------------------------------------------------------------------*/
-
 int main (void)
 {
 	FRESULT rc;				/* Result code */
 	DIR dir;				/* Directory object */
 	FILINFO fno;			/* File information object */
-	unsigned long bw, br, i;
-	int pos=0;
+	unsigned long br, i;
+	alt_u32* active_buff;
 
-	printf("hej\r\n");
-	// Switch all off
-    lcd_off();
+ 	printf("System started\r\n");
+ 	lcd_off();
     for(i=0;i<5000000;i++){}
 
-    // switch on the backlight
-    //
-
-    // initialize the DMA and get a device handle
-    //
     alt_sgdma_dev *dma = alt_avalon_sgdma_open("/dev/sgdma");
     printf("open dma returned %ld\n", (alt_u32)dma);
     printf("framebuffer 1 at %lx\n", (alt_u32)frameBufferA);
+    printf("framebuffer 2 at %lx\n", (alt_u32)frameBufferB);
+
+    active_buff = frameBufferA;
+    //printf("framebuffer 1 at %lx\n", (alt_u32)active_buff);
+
+    printf("Turning LCD on \r\n");
+    lcd_on();
+    printf("Starting DMA\r\n");
     init_and_start_framebuffer(dma);
 
-    lcd_on();
 	IoInit();
 
     Color col2;
-    col2.color32 = 0x00ffffff;
+    col2.color32 = 0x00ff0000;
 
-    nonburst_memset(frameBufferA, col2.color32,  522240/4);
+    memset(frameBufferA, col2.color32,  522240/4);
 
 	f_mount(0, &Fatfs);		/* Register volume work area (never fails) */
 
@@ -145,40 +147,24 @@ int main (void)
 			printf("   <dir>  %s\n", fno.fname);
 		else {
 			printf("%8lu  %s\n", fno.fsize, fno.fname);
-		printf("\nOpen an existing file.\n");
+		//printf("\nOpen an existing file.\n");
 		rc = f_open(&Fil, fno.fname, FA_READ);
 		if (rc) die(rc);
-		printf("\nType the file content.\n");
-		pos = 0;
+
+		if (active_buffer == 0) {
+			active_buffer = 1;
+			active_buff = frameBufferA;
+		} else {
+			active_buff = frameBufferB;
+			active_buffer = 0;
+		}
+
 		for (;;) {
-			i = 1000;
-			if (active_buffer == 0) {
-				rc = f_read(&Fil, (alt_u32*)frameBufferB+pos, 100, &br);	/* Read a chunk of file */
-				//memcpy(frameBufferB+pos, Buff, 10);
-				pos = pos+25;
-				while(i--){}
-				if (rc || !br) {
-					active_buffer = 1;
-					//pos = 0;
-					break;			/* Error or end of file */
-				}
-			} else {
-				rc = f_read(&Fil, (alt_u32*)frameBufferA+pos, 100, &br);	/* Read a chunk of file */
-				//memcpy(frameBufferB+pos, Buff, 10);
-				pos = pos+25;
-				while(i--){}
-				if (rc || !br) {
-					active_buffer = 0;
-					//pos = 0;
-					break;			/* Error or end of file */
-				}
-			}
-			//for (i = 0; i < br; i++)		/* Type the data */
-			//	putchar(Buff[i]);
+			rc = f_read(&Fil, (alt_u32*)active_buff, f_size(&Fil), &br);	/* Read a chunk of file */
+			if (rc || !br)
+				break;			/* Error or end of file */
 		}
 		if (rc) die(rc);
-
-		printf("\nClose the file.\n");
 		rc = f_close(&Fil);
 		if (rc) die(rc);
 		}
@@ -186,76 +172,6 @@ int main (void)
 	}
 	if (rc) die(rc);
 	}
-
-	printf("\nTest completed.\n");
-	for(i=0;i<=30000000;i++) {};
-	   // now we actually have some time to draw something
-	    //
-	    Particle particles[2] = { {100,0, -1, -1}, {0, 120, -1, -1} };
-	    Color col;
-	    int count;
-	    int x,y;
-	    int j = 0;
-	    int k = 255;
-	    int dire = 1;
-
-	    col.color32 = 0xffffffff;
-	    nonburst_memset(frameBufferA, col.color32, 522240/4);
-
-	    for(count=1;;count++) {
-
-	        // blink an led to see the program running
-	    	IOWR_ALTERA_AVALON_PIO_DATA(LED_PIO_BASE, (count & 0x0001)+128+64);
-
-	    	//MandelBrot(frameBufferA);
-	    	col.color8.r = 255;
-	    	col.color8.g = 255;
-	    	col.color8.b = 255;
-
-
-
-	    	for(i=0;i<=3000;i++) {};
-
-	    	if (count%5000 == 0) {
-	    		if(active_buffer == 0) {
-	    			active_buffer = 1;
-	    			nonburst_memset(frameBufferA, col.color32, 130560);
-	    		} else {
-	    			active_buffer = 0;
-	    			nonburst_memset(frameBufferB, col.color32, 130560);
-	    		}
-	    	}
-
-	    	if((j>=255) && (dire == 1)) {
-	    		dire = -1;
-	    	} else if((j <= 0) && (dire == -1)) {
-	    		dire = 1;
-	    	}
-
-	    	j += dire;
-	    	k += -1*dire;
-
-	    	// draw a line in a random color
-	    	col.color8.r = j;
-	    	col.color8.g = 0;
-	    	col.color8.b = 50;
-	    	if (active_buffer == 0) {
-	    		line(frameBufferA, particles[0].x, particles[0].y,
-	    				particles[1].x, particles[1].y, col);
-	    	} else {
-	      		line(frameBufferB, particles[0].x, particles[0].y,
-	        				particles[1].x, particles[1].y, col);
-	    	}
-
-	        // bounce the particles around
-	        int i;
-	        for(i = 0; i < 2; ++i) {
-	            if(particles[i].x == 0 || particles[i].x == 479) particles[i].vx *= -1;
-	            particles[i].x += particles[i].vx;
-	            if(particles[i].y == 0 || particles[i].y == 271) particles[i].vy *= -1;
-	            particles[i].y += particles[i].vy;
-	        }
-	    }
 }
 
 
